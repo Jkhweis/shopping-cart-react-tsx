40757fd1e2cee1c665b7a478781e4921298f34eb
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-09-07 10:34:50 -0500

    Fetch API Data

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,26 @@ import Badge from '@material-ui/core/Badge';
 //styles
 import { Wrapper } from './App.styles';
 
+//types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+  console.log(data);
   return <div className="App">Start</div>;
 };
 

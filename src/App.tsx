65a9cdc1863393af06ad37e7582c78a53360df1b
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-09-07 10:57:32 -0500

    Create Item Grid & handleAddToCart

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 //components
+import Item from './item/item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -32,7 +33,27 @@ const App = () => {
     getProducts
   );
   console.log(data);
-  return <div className="App">Start</div>;
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
